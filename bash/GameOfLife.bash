6a21cf996fe40cae829424abae3f161605ffcffe
Serelllka <44942855+Serelllka@users.noreply.github.com>
2021-10-08 00:56:34 +0300

    Update GameOfLife.bash

--- bash/GameOfLife.bash
+++ bash/GameOfLife.bash
@@ -8,8 +8,11 @@ number_of_neighbors() {
         for ((j=1;j<=$2;++j))
         do
             k=0
-            let inc_i=$i+1; let inc_j=$j+1
-            let dec_i=$i-1; let dec_j=$j-1
+            let inc_i=$((($i+$1)%$1))+1
+            let inc_j=$((($j+$2)%$2))+1
+            let dec_i=$((($i+$1-2)%$1))+1
+            let dec_j=$((($j+$2-2)%$2))+1
+
             if [[ ${matrix[$inc_i,$inc_j]} -eq 1 ]]
             then
                 let k=$k+1
@@ -107,10 +110,24 @@ do
     let j=$j+1
 done < input.txt
 
+for ((i=1;i<=$2;++i))
+do
+    cell=$(awk -v ind=$i '{printf($ind)}' <<< $line)
+    matrix[$j,$i]=$cell
+done
+
 counter=1
 for ((;counter<=$3;++counter))
 do
     write_to_file $1 $2 "$frames$counter"
     number_of_neighbors $1 $2
     next_generation $1 $2
+done
+
+
+for ((i=1;i<=$3;++i))
+do
+    clear
+    cat $frames$i
+    sleep 0.2
 done
